7e88987ca3e7130506743ae43e9fc7aae051bfaa
Saurabh Kumar Jha <100080003+Imsaurav1@users.noreply.github.com>
2024-07-07 00:53:10 +0530

    Add files via upload

---
+++ js/form.js
@@ -0,0 +1,51 @@
+document.getElementById('contactForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    var fullName = document.getElementById('fullName').value;
+    var email = document.getElementById('email').value;
+    var phone = document.getElementById('phone').value;
+    var subject = document.getElementById('subject').value;
+    var message = document.getElementById('message').value;
+
+    // Basic validation
+    if (!fullName || !email || !phone || !subject || !message) {
+        alert("All fields must be filled out.");
+        return;
+    }
+
+    // Email validation
+    var emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
+    if (!emailPattern.test(email)) {
+        alert("Please enter a valid email address.");
+        return;
+    }
+
+    Email.send({
+        SecureToken: "f2b9c068-5ec1-47ca-988e-003722c39766",
+        
+        To: 'bt21eee036@gmail.com',
+        From: email,
+        Subject: subject,
+        Body: `<b>Name:</b> ${fullName} <br>
+               <b>Email:</b> ${email} <br>
+               <b>Phone:</b> ${phone} <br>
+               <b>Message:</b> <br>${message}`
+    }).then(
+        response => {
+            Swal.fire({
+            icon: 'success',
+            title: 'Message Sent',
+            text: 'Your message has been sent successfully!'
+
+        })
+
+        document.getElementById('contactForm').reset();
+    }
+    ).catch(
+        error => Swal.fire({
+            icon: 'error',
+            title: 'Failed to Send',
+            text: 'Failed to send message. Please try again later.'
+        })
+    );
+});
