e3152cb2edbe6b4531652468714d34a8e64e4a93
Saurabh Kumar Jha <100080003+Imsaurav1@users.noreply.github.com>
2026-01-07 01:27:57 +0530

    Update posts.js

--- posts/posts.js
+++ posts/posts.js
@@ -1,4 +1,4 @@
-const API_BASE = "https://studymaterial-1heb.onrender.com"; 
+const API_BASE = "https://studymaterial-1heb.onrender.com/api/posts"; 
 // change if backend domain is different
 
 // ------------------------------
